b7eef718d0d2999612a1cc8f82ee8e0cd2759e78
21ccronk <21ccronk@athenian.org>
2020-09-15 20:55:23 +0000

    Finished calculator

--- calculator.py
+++ calculator.py
@@ -2,3 +2,39 @@
 Collaborators: 
 
 '''
+def add_function():
+    number1 = int(input("Enter a number: "))
+    number2 = int(input("Enter another number: "))
+    answer = number1 + number2
+    return answer
+
+def subtract_function():
+    number1 = int(input("Enter a number: "))
+    number2 = int(input("Enter another number: "))
+    answer = number1 - number2
+    return answer
+
+def multiply_function():
+    number1 = int(input("Enter a number: "))
+    number2 = int(input("Enter another number: "))
+    answer = number1 * number2
+    return answer
+
+def divide_function():
+    number1 = int(input("Enter a number: "))
+    number2 = int(input("Enter another number: "))
+    answer = number1 / number2
+    return answer
+
+operation = input("Enter 1 to add, 2 to subtract, 3 to multiply, and 4 to divide: ")
+if operation == "1":
+    print(add_function())
+
+elif operation == "2":
+    print(subtract_function())
+
+elif operation == "3":
+    print(multiply_function())
+
+elif operation == "4":
+    print(divide_function())
