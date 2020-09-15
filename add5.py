5f373a3069c4fe79ef1c792215e3088dc34efedc
21ccronk <21ccronk@athenian.org>
2020-09-15 20:25:36 +0000

    Finished task 3

--- add5.py
+++ add5.py
@@ -1,8 +1,9 @@
 def add_five(num1):
     print(num1 + 5)
 
-add_five(42)
-# user_input = int(input("Enter a number"))
-# add_five(user_input)
+# add_five(46)
 
-# add_five(int(input("Enter a number")))
+#user_input = int(input("Enter a number"))
+#add_five(user_input)
+
+add_five(int(input("Enter a number")))
