036f9866e8e0511d1ad66c5c8828b2bd5a5256d5
21ccronk <21ccronk@athenian.org>
2020-09-15 20:20:33 +0000

    Finished task 2

--- multiplier.py
+++ multiplier.py
@@ -4,3 +4,13 @@ The user should be able to input two numbers that are stored in variables.
 The function should multiply the two variables together and return the result to a variable in the main program.
 The main program should output the variable containing the result returned from the function.
 '''
+
+def multiplier():
+    number1 = int(input("Enter a number: "))
+    number2 = int(input("Enter anither number: "))
+    product = number1 * number2
+    return product
+    
+
+output_num = multiplier()
+print(output_num)
